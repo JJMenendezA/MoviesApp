0345144fcb8d98a204b0c3bde8fbe7db96c394f7
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2024-10-21 20:05:59 -0600

    Initial Commit

---
+++ Movies App/Movies_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_AppApp.swift
+//  Movies App
+//
+//  Created by Juan José Menéndez Alarcón on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Movies_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
