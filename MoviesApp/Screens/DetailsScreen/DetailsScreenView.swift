70b4e2848eca7d8f64b79c9aa591e672a022c87d
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2024-12-05 22:00:13 -0600

    ---- Added some dividers, contained all elements in a ScrollView and also added a header to the Details Screen View ----

--- MoviesApp/Screens/DetailsScreen/DetailsScreenView.swift
+++ MoviesApp/Screens/DetailsScreen/DetailsScreenView.swift
@@ -12,86 +12,144 @@ import Kingfisher
 struct DetailsScreenView: View {
     var movie: MovieInfo = dummyMovieInfo
     var body: some View {
-        ScrollView {
-            VStack {
-                KFImage(movieImageURL.appendingPathComponent(movie.poster_path))
-                    .resizable()
-                    .frame(width: 300, height: 400)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .shadow(radius: 10)
+        VStack {
+            
+            HStack(spacing: 0) {
+                Button(action: {}) {
+                    Image(systemName: "arrow.left")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                    
+                }
+              
+                Spacer()
                 
                 Text(movie.title)
-                    .foregroundStyle(.white)
-                    .font(.largeTitle)
+                    .font(.title)
                     .fontWeight(.bold)
-                    .padding()
-                
-                HStack {
-                    VStack {
-                        Text("Release Date:")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                            .foregroundStyle(.white)
+                    .foregroundStyle(.white)
+
+                Spacer()
+            } // :HStack
+            .padding(.horizontal)
+            
+            Divider()
+                .frame(height: 0.5)
+                .overlay(.white)
+                .padding(.horizontal)
+            
+            ScrollView {
+                VStack {
+                    KFImage(movieImageURL.appendingPathComponent(movie.poster_path))
+                        .resizable()
+                        .frame(width: 300, height: 400)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .shadow(radius: 10)
+
+                    Divider()
+                        .frame(height: 0.5)
+                        .overlay(.white)
+                        .padding(.horizontal)
+                        .padding(.top)
+                    
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Text("Release date:")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                                .padding(7)
+                                .background(customLinearGradient(colors: [.gray900, .purple900]))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                            
+                            Text(movie.releaseDateFormatted)
+                                .font(.body)
+                                .foregroundStyle(.white)
+                        } // :VStack
                         
-                        Text(movie.releaseDateFormatted)
-                            .font(.body)
-                            .foregroundStyle(.white)
-                    } // :VStack
+                        Spacer()
+                        
+                        Divider()
+                            .frame(width: 1)
+                            .overlay(.white)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Text("Original language:")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                                .padding(7)
+                                .background(customLinearGradient(colors: [.gray900, .purple900]))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                            
+                            Text(movie.originalLanguageComplete)
+                                .font(.body)
+                                .foregroundStyle(.white)
+                        } // :VStack
+                        
+                        Spacer()
+                    } // :HStack
+                    
+                    Divider()
+                        .frame(height: 0.5)
+                        .overlay(.white)
+                        .padding(.horizontal)
+                    
+                    Text("Overview:")
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .font(.title2)
+                        .padding(7)
+                        .background(customLinearGradient(colors: [.gray900, .purple900]))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                    
+                    Text(movie.overview)
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+                    
+                    Divider()
+                        .frame(height: 0.5)
+                        .overlay(.white)
+                        .padding(.horizontal)
+                    
+                    Text("Vote average:")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundStyle(.white)
+                        .padding(7)
+                        .background(customLinearGradient(colors: [.gray900, .purple900]))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                    
+                    HStack {
+                        ForEach (0..<movie.stars, id: \.self) { index in
+                            Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                        }
+                        
+                        if movie.hasHalfStar {
+                            Image(systemName: "star.leadinghalf.filled")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                        }
+                    } // :HStack
+                    .foregroundStyle(.white)
+                    .padding(.bottom, 10)
+                    .padding(.horizontal, 10)
+                    
+                    
                     
                     Spacer()
                     
-                    VStack {
-                        Text("Original language:")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                            .foregroundStyle(.white)
-                        
-                        Text(movie.originalLanguageComplete)
-                            .font(.body)
-                            .foregroundStyle(.white)
-                    } // :VStack
-                } // :HStack
-                .padding()
+                } // :VStack
                 
-                Text("Vote average:")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                
-                HStack {
-                    ForEach (0..<movie.stars, id: \.self) { index in
-                        Image(systemName: "star.fill")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                    }
-                    
-                    if movie.hasHalfStar {
-                        Image(systemName: "star.leadinghalf.filled")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                    }
-                } // :HStack
-                .foregroundStyle(.white)
-                .padding(.bottom, 10)
-                .padding(.horizontal, 10)
-                
-                
-                Text("Overview:")
-                    .foregroundStyle(.white)
-                    .fontWeight(.bold)
-                    .font(.title2)
-                
-                Text(movie.overview)
-                    .foregroundStyle(.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                
-                
-                Spacer()
-                
-            } // :VStack
-            
-        } // :ScrollView
+            } // :ScrollView
+        } // :VStack
         .frame(maxWidth: .infinity)
         .background(.gray900)
     }
