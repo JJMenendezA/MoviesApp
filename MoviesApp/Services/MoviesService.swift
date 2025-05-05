77983752c469c1eac274b5fb43198ddd87d92093
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-05-04 22:48:59 -0600

    ---- Refactored all the functions in the MoviesService into using the async/await structure ----

--- MoviesApp/Services/MoviesService.swift
+++ MoviesApp/Services/MoviesService.swift
@@ -15,76 +15,31 @@ class MoviesService: MoviesServiceProtocol {
     init(networkManager: NetworkManager = NetworkManager.shared) {
         self.networkManager = networkManager
     }
-        
-    func fetchAllMovies(completion: @escaping (Result<[String: MoviesResponse], AppError>) -> Void) {
+    
+    func fetchAllMovies() async throws -> [String: MoviesResponse] {
         var moviesList: [String: MoviesResponse] = [:]
-        var moviesError: AppError?
-        
-        let dispatchGroup = DispatchGroup()
         
         for type in MovieTypes.allCases {
-            dispatchGroup.enter()
-            fetchMovies(endpoint: type.endpoint, completion: { result in
-                switch result {
-                case .success(let fetchedMovies):
-                    moviesList[type.title] = fetchedMovies
-                case .failure(let error):
-                    moviesError = error
-                }
-                dispatchGroup.leave()
-            })
+            moviesList[type.title] = try await fetchMovies(endpoint: type.endpoint)
         }
-        
-        dispatchGroup.notify(queue: .main) {
-            if moviesError == nil {
-                completion(.success(moviesList))
-            } else {
-                if let error = moviesError {
-                    completion(.failure(error))
-                }
-            }
-        }
+        return moviesList
     }
     
-    func fetchMovies(endpoint: String,
-                     completion: @escaping (Result<MoviesResponse, AppError>) -> Void) {
-        networkManager.getMoviesRequest(endpoint: endpoint,
-                                        queryItems: [URLQueryItem(name: "language",
-                                                                  value: language),
-                                                     URLQueryItem(name: "page",
-                                                                  value: "1")],
-                                        response: MoviesResponse.self) { result in
-            switch result {
-            case .success(let fetchedMovies):
-                DispatchQueue.main.async {
-                    completion(.success(fetchedMovies))
-                }
-            case .failure(let error):
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }
+    func fetchMovies(endpoint: String)  async throws -> MoviesResponse {
+        try await networkManager.getMoviesRequest(endpoint: endpoint,
+        queryItems: [URLQueryItem(name: "language",
+                                  value: language),
+                     URLQueryItem(name: "page",
+                                  value: "1")],
+        response: MoviesResponse.self)
     }
     
-    func fetchMovieDetails(endPoint: String,
-                           completion: @escaping (Result<MovieDetailsResponse, AppError>) -> Void) {
-        networkManager.getMoviesRequest(endpoint: endPoint,
-                                        queryItems: [URLQueryItem(name: "append_to_response",
-                                                                  value: "videos,similar"),
-                                                     URLQueryItem(name: "language",
-                                                                  value: language)],
-                                        response: MovieDetailsResponse.self) { result in
-            switch result {
-            case .success(let fetchedMovies):
-                DispatchQueue.main.async {
-                    completion(.success(fetchedMovies))
-                }
-            case .failure(let error):
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }
+    func fecthMovieDetails(endPoint: String) async throws -> MovieDetailsResponse {
+        try await networkManager.getMoviesRequest(endpoint: endPoint,
+                                                  queryItems: [URLQueryItem(name: "append_to_response",
+                                                                            value: "videos,similar"),
+                                                               URLQueryItem(name: "language",
+                                                                            value: language)],
+                                                  response: MovieDetailsResponse.self)
     }
 }
