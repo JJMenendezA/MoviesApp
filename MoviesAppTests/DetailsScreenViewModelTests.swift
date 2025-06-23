54293f0911ccd3297f647514abf72a902b7c8490
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-06-22 20:31:03 -0600

    ---- Created the view model tests files and implemented the respective tests ----

---
+++ MoviesAppTests/DetailsScreenViewModelTests.swift
@@ -0,0 +1,43 @@
+//
+//  DetailsScreenViewModelTests.swift
+//  MoviesAppTests
+//
+//  Created by Juan José Menéndez Alarcón on 22/06/25.
+//  Copyright © 2025 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import XCTest
+@testable import MoviesApp
+
+final class DetailsScreenViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_fetchDetails_successfullyCompletes() async throws {
+        // Arrange
+        let useCase = MockFetchMovieDetailsUseCase()
+        let sut = DetailsScreenViewModel(fetchMovieDetailsUseCase: useCase)
+        // Act
+        await sut.fetchMovieDetails(movieId: 0)
+        XCTAssertFalse(sut.isLoading, "Loading state should be false")
+        XCTAssertFalse(sut.movieDetails == nil, "MovieDetails shouldn't be nil")
+    }
+    
+    func test_fetchDetails_failsWithError() async throws {
+        // Arrange
+        let useCase = MockFetchMovieDetailsUseCase()
+        let sut = DetailsScreenViewModel(fetchMovieDetailsUseCase: useCase)
+        useCase.shouldFail = true
+        // Act
+        await sut.fetchMovieDetails(movieId: 0)
+        XCTAssertFalse(sut.isLoading, "Loading state should be false after fetch completes")
+        XCTAssertTrue(sut.hasErrorTriggered, "HasErrorTriggered state should be true after fetch fails with error")
+        XCTAssertEqual(sut.error, AppError.noData)
+    }
+}
---
+++ MoviesAppTests/MainScreenViewModelTests.swift
@@ -0,0 +1,46 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppMainVieModelTests
+//
+//  Created by Juan José Menéndez Alarcón on 21/11/24.
+//  Copyright © 2024 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import XCTest
+@testable import MoviesApp
+
+final class MainScreenViewModelTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_fetchAllMovies_successfullyCompletes() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        // Act
+        await sut.fetchMovies()
+        XCTAssertFalse(sut.isLoading, "Loading state should be false after fetch completes")
+        XCTAssertFalse(sut.moviesDictionary.isEmpty, "Dictionary shouldn't be empty after fetching")
+        XCTAssertFalse(sut.originalLanguagesList.isEmpty, "List shouldn't be empty after fetching")
+        XCTAssertFalse(sut.releaseDatesList.isEmpty, "List shouldn't be empty after fetching")
+    }
+    
+    func test_fetchAllMovies_failsWithError() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        useCase.shouldFail = true
+        // Act
+        XCTAssertTrue(sut.isLoading, "Loading state should be true before fetch completes")
+        await sut.fetchMovies()
+        XCTAssertFalse(sut.isLoading, "Loading state should be false after fetch completes")
+        XCTAssertTrue(sut.hasErrorTriggered, "HasErrorTriggered state should be true after fetch fails with error")
+        XCTAssertEqual(sut.error, AppError.noData)
+    }
+}
