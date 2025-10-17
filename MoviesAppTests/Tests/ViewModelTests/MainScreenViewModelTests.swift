aaf1f219faae00fa710b24f41b2850bf3a66632a
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-10-16 20:52:53 -0600

    Added two scenarios in the MainScreenViewModelTests in order to test the setting of the languages and dates arrays after fetching movies.

--- MoviesAppTests/Tests/ViewModelTests/MainScreenViewModelTests.swift
+++ MoviesAppTests/Tests/ViewModelTests/MainScreenViewModelTests.swift
@@ -40,4 +40,47 @@ final class MainScreenViewModelTests: XCTestCase {
         XCTAssertTrue(sut.hasErrorBeenTriggered, "HasErrorBeenTriggered state should be true after fetch fails with error")
         XCTAssertEqual(sut.error, AppError.noData)
     }
+    
+    func test_setDateArray_succcessfullyCompletes() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        // Act
+        await sut.fetchMovies()
+        sut.setDateArray()
+        XCTAssertFalse(sut.releaseDatesArray.isEmpty, "Array shouldn't be empty after fetching")
+    }
+    
+    func test_setDateArray_failsToConclude() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        useCase.shouldFail = true
+        // Act
+        XCTAssertTrue(sut.isInformationLoading, "Loading state should be true before fetch completes")
+        await sut.fetchMovies()
+        sut.setDateArray()
+        XCTAssertTrue(sut.releaseDatesArray.isEmpty, "Array should be empty after error")
+    }
+    
+    func test_setLanguageArray_successfullyCompletes() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        // Act
+        await sut.fetchMovies()
+        sut.setLanguageArray()
+        XCTAssertFalse(sut.languagesArray.isEmpty, "Array shouldn't be empty after fetching")
+    }
+    
+    func test_setLanguageArray_failsToConclude() async throws {
+        // Arrange
+        let useCase = MockFetchMoviesUseCase()
+        let sut = MainScreenViewModel(fetchMoviesUseCase: useCase)
+        useCase.shouldFail = true
+        // Act
+        await sut.fetchMovies()
+        sut.setLanguageArray()
+        XCTAssertEqual(sut.languagesArray, ["All languages"], "Array should only have All languages in it")
+    }
 }
