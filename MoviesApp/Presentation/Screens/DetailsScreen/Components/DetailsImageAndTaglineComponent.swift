34aa2c90de441fc91b56387ea1f61ae5ead79e2c
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-10-30 21:13:55 -0600

    Added the animated effect to the image and the handling of the multi images coming from the previous screen.

--- MoviesApp/Presentation/Screens/DetailsScreen/Components/DetailsImageAndTaglineComponent.swift
+++ MoviesApp/Presentation/Screens/DetailsScreen/Components/DetailsImageAndTaglineComponent.swift
@@ -10,31 +10,67 @@ import SwiftUI
 import Kingfisher
 
 struct DetailsImageAndTaglineComponent: View {
-    var moviePoster: String?
+    var moviePosters: [String?]
     var tagline: String
+    @State var xScale: CGFloat = 1
+    @State var multiMoviePoster: String = ""
     var body: some View {
-        if let moviePosterPath = moviePoster,
-            let movieImageURL = URL(string: "https://image.tmdb.org/t/p/w500") {
-            KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
-                .resizable()
-                .frame(width: 300, height: 425)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .shadow(radius: 10)
-                .padding(.bottom, 20)
+        VStack {
+            if moviePosters.count > 1 {
+                if let moviePosterPath = moviePosters[0],
+                   let alternativeImagePath = moviePosters[1],
+                   let movieImageURL = URL(string: "https://image.tmdb.org/t/p/w500") {
+                    Button(action: {
+                        withAnimation {
+                            if xScale < 0 {
+                                xScale = 1
+                                multiMoviePoster = moviePosterPath
+                            } else {
+                                xScale = -1
+                                multiMoviePoster = alternativeImagePath
+                            }
+                        }
+                    }, label: {
+                        KFImage(movieImageURL.appendingPathComponent(multiMoviePoster))
+                            .resizable()
+                            .frame(width: 300, height: 425)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .shadow(radius: 10)
+                            .padding(.bottom, 20)
+                            .scaleEffect(x: xScale, y: 1)
+                    })
+                }
+            } else {
+                if let moviePosterPath = moviePosters[0],
+                   let movieImageURL = URL(string: "https://image.tmdb.org/t/p/w500") {
+                    KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
+                        .resizable()
+                        .frame(width: 300, height: 425)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .shadow(radius: 10)
+                        .padding(.bottom, 20)
+                }
+            }
+            
+            Text(tagline)
+                .italic()
+                .fontWeight(.heavy)
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, tagline.isEmpty ? 0 : 20)
+                .lineLimit(4)
+                .minimumScaleFactor(0.5)
+        } // :VStack
+        .onAppear {
+            if !moviePosters.isEmpty {
+                if let moviePosterPath = moviePosters[0] {
+                    multiMoviePoster = moviePosterPath
+                }
+            }
         }
-        
-        Text(tagline)
-            .italic()
-            .fontWeight(.heavy)
-            .foregroundStyle(.white)
-            .multilineTextAlignment(.center)
-            .padding(.bottom, tagline.isEmpty ? 0 : 20)
-            .lineLimit(4)
-            .minimumScaleFactor(0.5)
-        
     }
 }
 
 #Preview {
-    DetailsImageAndTaglineComponent(moviePoster: "", tagline: "Example")
+    DetailsImageAndTaglineComponent(moviePosters: ["/63xYQj1BwRFielxsBDXvHIJyXVm.jpg", "/18TSJF1WLA4CkymvVUcKDBwUJ9F.jpg"], tagline: "Example")
 }
