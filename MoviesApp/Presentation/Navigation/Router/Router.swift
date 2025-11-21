e7cd3463945f7f4028adeeeebb2f17abbf1045ba
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-11-21 17:08:42 -0600

    Created a Router to centralize the navigations and be able to navigate from every view in the app.

---
+++ MoviesApp/Presentation/Navigation/Router/Router.swift
@@ -0,0 +1,17 @@
+//
+//  Router.swift
+//  MoviesApp
+//
+//  Created by Juan José Menéndez Alarcón on 21/11/25.
+//  Copyright © 2025 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import Foundation
+
+class Router: ObservableObject {
+    @Published var path: [Routes] = []
+    
+    func navigateTo(_ route: Routes) {
+        path.append(route as Routes)
+    }
+}
