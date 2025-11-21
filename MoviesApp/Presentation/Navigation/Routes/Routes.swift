d98ad74e46ab641e038443c8002c901b9a7c3c0d
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-11-21 17:07:17 -0600

    Created the Routes enum in order to be able to centralize the different routes of the app.

---
+++ MoviesApp/Presentation/Navigation/Routes/Routes.swift
@@ -0,0 +1,13 @@
+//
+//  Routes.swift
+//  MoviesApp
+//
+//  Created by Juan José Menéndez Alarcón on 21/11/25.
+//  Copyright © 2025 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import Foundation
+
+enum Routes: Hashable {
+    case details(id: Int)
+}
