390c1475ca48ae4f6c361f281acd4c8100f00c4e
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2025-11-24 16:13:34 -0600

    Refactored the MainScreenViewModel to MainViewModel.

--- MoviesAppTests/Tests/ViewModelTests/MainViewModelTests.swift
+++ MoviesAppTests/Tests/ViewModelTests/MainViewModelTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import MoviesApp
 
-final class MainScreenViewModelTests: XCTestCase {
+final class MainViewModelTests: XCTestCase {
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
