6665a069b29053d459e12714aeb4efdefe08cda3
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-01-08 12:16:42 -0600

    Missing commit with the completion of the FiltersParameter file.

--- MoviesApp/Presentation/ObservableObjects/FilterParameters.swift
+++ MoviesApp/Presentation/ObservableObjects/FilterParameters.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2026 Juan José Menéndez Alarcón. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+class FilterParameters: ObservableObject {
+    private var defaultLanguage: String = NSLocalizedString("All languages", comment: "")
+    private var defaultStartDate: Date = Date()
+    private var defaultEndDate: Date = Date()
+    
+    @Published var language: String = NSLocalizedString("All languages", comment: "")
+    @Published var startDate: Date = Date()
+    @Published var endDate: Date = Date()
+    
+    var areFiltersApplied: Bool {
+        language != defaultLanguage ||
+        startDate != defaultStartDate ||
+        endDate != defaultEndDate
+        }
+    
+    func setDefaultValues(startDate: Date, endDate: Date) {
+        self.startDate = startDate
+        defaultStartDate = startDate
+        self.endDate = endDate
+        defaultEndDate = endDate
+    }
+    
+    func cleanFilters() {
+        language = defaultLanguage
+        startDate = defaultStartDate
+        endDate = defaultEndDate
+    }
+}
