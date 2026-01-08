5225c2bec780b305fdc56e43650a6129e679cc75
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-01-08 12:17:56 -0600

    Created the AppSettings observable class in order to show and hide the ratings of the movie items in the app.

---
+++ MoviesApp/Presentation/ObservableObjects/AppSettings.swift
@@ -0,0 +1,13 @@
+//
+//  AppSettings.swift
+//  MoviesApp
+//
+//  Created by Juan José Menéndez Alarcón on 08/01/26.
+//  Copyright © 2026 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import SwiftUI
+
+class AppSettings: ObservableObject {
+    @Published var isShowingRating: Bool = true
+}
