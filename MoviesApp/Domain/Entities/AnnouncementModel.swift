bdbbf2f8c8cf537fc58798cdb5fd716ae669895c
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-02-02 13:17:29 -0600

    Fix: Fixed broken link int he AnnouncementModel and added new announcement.

--- MoviesApp/Domain/Entities/AnnouncementModel.swift
+++ MoviesApp/Domain/Entities/AnnouncementModel.swift
@@ -8,4 +8,5 @@
 
 let announcementsList: [String] = ["https://m.media-amazon.com/images/I/615UsSWBHxL._AC_UF894,1000_QL80_.jpg",
                                    "https://www.stophateuk.org/wp-content/uploads/2022/10/Stop-Hate-Racism_skin-colour-isnt-a-crime-1x1-1.png",
-                                   "https://www.csee-etuce.org/images/Pictures/Stop-VAW.png"]
+                                   "https://etuceprodstorage.blob.core.windows.net/assets/image/1624/2605/lead.jpg?se=3026-02-01T23:17:58Z&sv=2024-08-04&sr=b&sig=GZcm05tXxupE57zY6mQZEDJdviVZRmWOXqIYnr8%2FtIQ%3D&sp=r",
+                                    "https://muselot.in/cdn/shop/products/Love-is-love-posters-design_2048x.jpg?v=1657309680"]
