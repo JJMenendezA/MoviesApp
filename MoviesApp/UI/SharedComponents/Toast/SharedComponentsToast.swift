84229a407ca038245d8d7d83ae82176656299b83
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-02-02 14:42:40 -0600

    Feat: Modified the SharedComponentsToast to have a LocalizedStringResource as a parameter.

--- MoviesApp/UI/SharedComponents/Toast/SharedComponentsToast.swift
+++ MoviesApp/UI/SharedComponents/Toast/SharedComponentsToast.swift
@@ -9,18 +9,34 @@
 import SwiftUI
 
 struct SharedComponentsToast: View {
-    var text: String
+    var text: String?
+    var textLocalized: LocalizedStringResource?
     @Binding var isToastActive: Bool
     init(text: String,
          isToastActive: Binding<Bool>) {
         self.text = text
         self._isToastActive = Binding(projectedValue: isToastActive)
     }
+    init(textLocalized: LocalizedStringResource,
+         isToastActive: Binding<Bool>) {
+        self.textLocalized = textLocalized
+        self._isToastActive = Binding(projectedValue: isToastActive)
+    }
+    @ViewBuilder
+    var textComponent: some View {
+        if let notLocalizedText = text {
+            Text(notLocalizedText)
+        } else if let localizedText = textLocalized {
+            Text(localizedText)
+        } else {
+            EmptyView()
+        }
+    }
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
             .fill(.black)
             .overlay {
-                Text(text)
+                textComponent
             }
             .frame(height: 50)
             .padding()
