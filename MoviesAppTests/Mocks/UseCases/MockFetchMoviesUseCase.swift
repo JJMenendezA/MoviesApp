69642872164f733151cb743ef6b3a74350e8163a
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-27 13:39:00 -0600

    Fix: Updated the MockFetchMoviesUseCase with the new entity type.

--- MoviesAppTests/Mocks/UseCases/MockFetchMoviesUseCase.swift
+++ MoviesAppTests/Mocks/UseCases/MockFetchMoviesUseCase.swift
@@ -9,15 +9,11 @@
 class MockFetchMoviesUseCase: FetchMoviesUseCase {
     var shouldFail: Bool = false
     
-    func fetch() async throws -> [String: MoviesResponse] {
+    func fetch() async throws -> [String: MoviesEntity] {
         if shouldFail {
             throw AppError.noData
         } else {
-            return ["popular": MoviesResponse(dates: nil,
-                                              page: 1,
-                                              results: [dummyMovieResponse],
-                                              total_pages: 5,
-                                              total_results: 100)]
+            return ["popular": MoviesEntity(movies: [dummyMovieResponse])]
         }
     }
 }
