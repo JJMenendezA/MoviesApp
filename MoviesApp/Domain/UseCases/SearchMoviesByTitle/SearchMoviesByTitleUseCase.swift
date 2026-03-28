e24fefd5aa789b3a87f8bef3126143d36499d103
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-27 20:49:04 -0600

    Feat: Created the SearchMoviesByTitleUseCase.

---
+++ MoviesApp/Domain/UseCases/SearchMoviesByTitle/SearchMoviesByTitleUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  SearchMoviesByTitleUseCase.swift
+//  MoviesApp
+//
+//  Created by Juan José Menéndez Alarcón on 27/03/26.
+//  Copyright © 2026 Juan José Menéndez Alarcón. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchMoviesByTitleUseCase {
+    func search(moviesDictionary: [String: MoviesEntity], title: String) -> [String: MoviesEntity]
+}
+
+class SearchMoviesByTitleUseCaseImpl: SearchMoviesByTitleUseCase {
+    func search(moviesDictionary: [String : MoviesEntity], title: String) -> [String: MoviesEntity] {
+        var mutableMoviesDictionary: [String: MoviesEntity] = moviesDictionary
+        mutableMoviesDictionary.forEach({ movie in
+            mutableMoviesDictionary[movie.key]?.moviesArray = movie.value.moviesArray.filter({ movie in
+                movie.title.localizedCaseInsensitiveContains(title)
+            })
+        })
+        
+        return mutableMoviesDictionary
+    }
+}
