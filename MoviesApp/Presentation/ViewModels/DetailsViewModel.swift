bee066aa831362f64110ddd366ec0c667d57d2cd
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 12:15:56 -0600

    Fix: Removed the mapping from the DetailsViewModel.

--- MoviesApp/Presentation/ViewModels/DetailsViewModel.swift
+++ MoviesApp/Presentation/ViewModels/DetailsViewModel.swift
@@ -26,9 +26,7 @@ class DetailsViewModel: ObservableObject {
     func fetchMovieDetails(movieId: Int) async {
         isLoading = true
         do {
-            movieDetails = try await MovieDetailsEntity(from:
-                                                            fetchMovieDetailsUseCase.fetch(endPoint:
-                                                                                            MoviePathTypes.details(movieId: movieId).endpoint))
+            movieDetails = try await fetchMovieDetailsUseCase.fetch(endPoint: MoviePathTypes.details(movieId: movieId).endpoint)
             isLoading = false
         } catch let error as AppError {
             self.error = error
