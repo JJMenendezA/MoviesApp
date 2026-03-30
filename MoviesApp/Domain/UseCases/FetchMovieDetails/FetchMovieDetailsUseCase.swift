db24f6d18b74cc6cddf2cfe806016f3432eb4c60
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 13:20:36 -0600

    Fix: Removed mapping from FetchMovieDetailsUseCase.

--- MoviesApp/Domain/UseCases/FetchMovieDetails/FetchMovieDetailsUseCase.swift
+++ MoviesApp/Domain/UseCases/FetchMovieDetails/FetchMovieDetailsUseCase.swift
@@ -19,35 +19,6 @@ class FetchMovieDetailsImpl: FetchMovieDetailsUseCase {
     }
     
     func fetch(endPoint: String) async throws -> MovieDetailsEntity {
-        let rawData = try await repository.fetchDetails(endPoint: endPoint)
-
-        let genreList = rawData.genres.map(\.name).joined(separator: ", ")
-        let similarMoviesArray = rawData.similar.results.map({ movie in
-            MovieEntity(id: movie.id,
-                        posterPath: movie.poster_path,
-                        releaseDate: movie.release_date,
-                        title: movie.title,
-                        originalLanguage: movie.original_language,
-                        voteAverage: movie.vote_average)
-        })
-        let productionCompaniesArray = rawData.production_companies.map({ productionCompany in
-            ProductionCompanyEntity(name: productionCompany.name,
-                                    logoPath: productionCompany.logo_path,
-                                    country: productionCompany.origin_country)
-        })
-        
-        return MovieDetailsEntity(id: rawData.id,
-                                  title: rawData.title,
-                                  posterPath: rawData.poster_path,
-                                  alternativeImage: rawData.backdrop_path,
-                                  tagline: rawData.tagline,
-                                  releaseDate: rawData.release_date,
-                                  voteAverage: rawData.vote_average,
-                                  originalLanguage: rawData.original_language,
-                                  genreList: genreList,
-                                  runTime: rawData.runtime,
-                                  overview: rawData.overview,
-                                  similarMoviesList: similarMoviesArray,
-                                  productionCompanies: productionCompaniesArray)
+        try await repository.fetchDetails(endPoint: endPoint)
     }
 }
