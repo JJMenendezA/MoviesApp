dce9f8515ff832455ce92293bcdd1877aeb584ca
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 12:14:48 -0600

    Fix: Added mapping to the MockFetchMovieDetailsUseCase.

--- MoviesAppTests/Mocks/UseCases/MockFetchMovieDetailsUseCase.swift
+++ MoviesAppTests/Mocks/UseCases/MockFetchMovieDetailsUseCase.swift
@@ -5,15 +5,43 @@
 //  Created by Juan José Menéndez Alarcón on 26/05/25.
 //  Copyright © 2025 Juan José Menéndez Alarcón. All rights reserved.
 //
+import Foundation
 
 class MockFetchMovieDetailsUseCase: FetchMovieDetailsUseCase {
     var shouldFail: Bool = false
     
-    func fetch(endPoint: String) async throws -> MovieDetailsResponse {
+    func fetch(endPoint: String) async throws -> MovieDetailsEntity {
         if shouldFail {
             throw AppError.noData
         } else {
-            return dummyDetailsMovieInfo
+            let genreList = dummyDetailsMovieInfo.genres.map(\.name).joined(separator: ", ")
+            let similarMoviesArray = dummyDetailsMovieInfo.similar.results.map({ movie in
+                MovieEntity(id: movie.id,
+                            posterPath: movie.poster_path,
+                            releaseDate: movie.release_date,
+                            title: movie.title,
+                            originalLanguage: movie.original_language,
+                            voteAverage: movie.vote_average)
+            })
+            let productionCompaniesArray = dummyDetailsMovieInfo.production_companies.map({ productionCompany in
+                ProductionCompanyEntity(name: productionCompany.name,
+                                        logoPath: productionCompany.logo_path,
+                                        country: productionCompany.origin_country)
+            })
+            
+            return MovieDetailsEntity(id: dummyDetailsMovieInfo.id,
+                                      title: dummyDetailsMovieInfo.title,
+                                      posterPath: dummyDetailsMovieInfo.poster_path,
+                                      alternativeImage: dummyDetailsMovieInfo.backdrop_path,
+                                      tagline: dummyDetailsMovieInfo.tagline,
+                                      releaseDate: dummyDetailsMovieInfo.release_date,
+                                      voteAverage: dummyDetailsMovieInfo.vote_average,
+                                      originalLanguage: dummyDetailsMovieInfo.original_language,
+                                      genreList: genreList,
+                                      runTime: dummyDetailsMovieInfo.runtime,
+                                      overview: dummyDetailsMovieInfo.overview,
+                                      similarMoviesList: similarMoviesArray,
+                                      productionCompanies: productionCompaniesArray)
         }
     }
 }
