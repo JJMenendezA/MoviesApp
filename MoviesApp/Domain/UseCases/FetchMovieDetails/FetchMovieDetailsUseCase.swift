5ad06b00024ee6b981dc06f9d9a2df54e26ade72
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 12:13:14 -0600

    Fix: Added mapping tot he FetchMovieDetailsUseCase.

--- MoviesApp/Domain/UseCases/FetchMovieDetails/FetchMovieDetailsUseCase.swift
+++ MoviesApp/Domain/UseCases/FetchMovieDetails/FetchMovieDetailsUseCase.swift
@@ -5,9 +5,10 @@
 //  Created by Juan José Menéndez Alarcón on 25/05/25.
 //  Copyright © 2025 Juan José Menéndez Alarcón. All rights reserved.
 //
+import Foundation
 
 protocol FetchMovieDetailsUseCase {
-    func fetch(endPoint: String) async throws -> MovieDetailsResponse
+    func fetch(endPoint: String) async throws -> MovieDetailsEntity
 }
 
 class FetchMovieDetailsImpl: FetchMovieDetailsUseCase {
@@ -17,7 +18,36 @@ class FetchMovieDetailsImpl: FetchMovieDetailsUseCase {
         self.repository = repository
     }
     
-    func fetch(endPoint: String) async throws -> MovieDetailsResponse {
-        try await repository.fetchDetails(endPoint: endPoint)
+    func fetch(endPoint: String) async throws -> MovieDetailsEntity {
+        let rawData = try await repository.fetchDetails(endPoint: endPoint)
+
+        let genreList = rawData.genres.map(\.name).joined(separator: ", ")
+        let similarMoviesArray = rawData.similar.results.map({ movie in
+            MovieEntity(id: movie.id,
+                        posterPath: movie.poster_path,
+                        releaseDate: movie.release_date,
+                        title: movie.title,
+                        originalLanguage: movie.original_language,
+                        voteAverage: movie.vote_average)
+        })
+        let productionCompaniesArray = rawData.production_companies.map({ productionCompany in
+            ProductionCompanyEntity(name: productionCompany.name,
+                                    logoPath: productionCompany.logo_path,
+                                    country: productionCompany.origin_country)
+        })
+        
+        return MovieDetailsEntity(id: rawData.id,
+                                  title: rawData.title,
+                                  posterPath: rawData.poster_path,
+                                  alternativeImage: rawData.backdrop_path,
+                                  tagline: rawData.tagline,
+                                  releaseDate: rawData.release_date,
+                                  voteAverage: rawData.vote_average,
+                                  originalLanguage: rawData.original_language,
+                                  genreList: genreList,
+                                  runTime: rawData.runtime,
+                                  overview: rawData.overview,
+                                  similarMoviesList: similarMoviesArray,
+                                  productionCompanies: productionCompaniesArray)
     }
 }
