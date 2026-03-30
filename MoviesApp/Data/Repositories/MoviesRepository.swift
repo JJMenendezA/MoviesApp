f6b4fa1762988c6943fda2ffe07026160afc5b0c
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 13:19:17 -0600

    Fix: Moved the mapping of the entities into the repository.

--- MoviesApp/Data/Repositories/MoviesRepository.swift
+++ MoviesApp/Data/Repositories/MoviesRepository.swift
@@ -7,8 +7,8 @@
 //
 
 protocol MoviesRepository {
-    func fetchMovies() async throws -> [String: MoviesResponse]
-    func fetchDetails(endPoint: String) async throws -> MovieDetailsResponse
+    func fetchMovies() async throws -> [String: MoviesEntity]
+    func fetchDetails(endPoint: String) async throws -> MovieDetailsEntity
 }
 
 class MoviesRepositoryImpl: MoviesRepository {
@@ -18,11 +18,53 @@ class MoviesRepositoryImpl: MoviesRepository {
         self.moviesService = moviesService
     }
     
-    func fetchMovies() async throws -> [String: MoviesResponse] {
-        try await moviesService.fetchAllMovies()
+    func fetchMovies() async throws -> [String: MoviesEntity] {
+        let rawData = try await moviesService.fetchAllMovies()
+        var mutableMoviesDictionary: [String: MoviesEntity] = [:]
+        rawData.forEach({ movie in
+            mutableMoviesDictionary[movie.key] = MoviesEntity(movieEntities: movie.value.results.map({ movie in
+                MovieEntity(id: movie.id,
+                            posterPath: movie.poster_path,
+                            releaseDate: movie.release_date,
+                            title: movie.title,
+                            originalLanguage: movie.original_language,
+                            voteAverage: movie.vote_average)
+            }))
+        })
+        
+        return mutableMoviesDictionary
     }
     
-    func fetchDetails(endPoint: String) async throws -> MovieDetailsResponse {
-        try await moviesService.fecthMovieDetails(endPoint: endPoint)
+    func fetchDetails(endPoint: String) async throws -> MovieDetailsEntity {
+        let rawData = try await moviesService.fetchMovieDetails(endPoint: endPoint)
+
+        let genreList = rawData.genres.map(\.name).joined(separator: ", ")
+        let similarMoviesArray = rawData.similar.results.map({ movie in
+            MovieEntity(id: movie.id,
+                        posterPath: movie.poster_path,
+                        releaseDate: movie.release_date,
+                        title: movie.title,
+                        originalLanguage: movie.original_language,
+                        voteAverage: movie.vote_average)
+        })
+        let productionCompaniesArray = rawData.production_companies.map({ productionCompany in
+            ProductionCompanyEntity(name: productionCompany.name,
+                                    logoPath: productionCompany.logo_path,
+                                    country: productionCompany.origin_country)
+        })
+        
+        return MovieDetailsEntity(id: rawData.id,
+                                  title: rawData.title,
+                                  posterPath: rawData.poster_path,
+                                  alternativeImage: rawData.backdrop_path,
+                                  tagline: rawData.tagline,
+                                  releaseDate: rawData.release_date,
+                                  voteAverage: rawData.vote_average,
+                                  originalLanguage: rawData.original_language,
+                                  genreList: genreList,
+                                  runTime: rawData.runtime,
+                                  overview: rawData.overview,
+                                  similarMoviesList: similarMoviesArray,
+                                  productionCompanies: productionCompaniesArray)
     }
 }
