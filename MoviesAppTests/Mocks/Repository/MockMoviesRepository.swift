d54f6372192990e3c9f44e00bad29e7794ce50bd
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-03-30 13:22:50 -0600

    Fix: Moved mapping into the MockMoviesRepository.

--- MoviesAppTests/Mocks/Repository/MockMoviesRepository.swift
+++ MoviesAppTests/Mocks/Repository/MockMoviesRepository.swift
@@ -13,23 +13,51 @@ class MockMoviesRepository: MoviesRepository {
         self.moviesService = moviesService
     }
     
-    func fetchMovies() async throws -> [String: MoviesResponse] {
+    func fetchMovies() async throws -> [String: MoviesEntity] {
         if shouldFail {
             throw AppError.noData
         } else {
-            return ["popular": MoviesResponse(dates: nil,
-                                              page: 1,
-                                              results: [dummyMovieResponse],
-                                              total_pages: 5,
-                                              total_results: 100)]
+            return ["popular": MoviesEntity(movieEntities: [MovieEntity(id: dummyMovieResponse.id,
+                                                                        posterPath: dummyMovieResponse.poster_path,
+                                                                        releaseDate: dummyMovieResponse.release_date,
+                                                                        title: dummyMovieResponse.title,
+                                                                        originalLanguage: dummyMovieResponse.original_language,
+                                                                        voteAverage: dummyMovieResponse.vote_average)])]
         }
     }
     
-    func fetchDetails(endPoint: String) async throws -> MovieDetailsResponse {
+    func fetchDetails(endPoint: String) async throws -> MovieDetailsEntity {
         if shouldFail {
             throw AppError.noData
         } else {
-            return dummyDetailsMovieInfo
+            let genreList = dummyDetailsMovieInfo.genres.map(\.name).joined(separator: ", ")
+            let similarMoviesArray = dummyDetailsMovieInfo.similar.results.map({ movie in
+                MovieEntity(id: movie.id,
+                            posterPath: movie.poster_path,
+                            releaseDate: movie.release_date,
+                            title: movie.title,
+                            originalLanguage: movie.original_language,
+                            voteAverage: movie.vote_average)
+            })
+            let productionCompaniesArray = dummyDetailsMovieInfo.production_companies.map({ productionCompany in
+                ProductionCompanyEntity(name: productionCompany.name,
+                                        logoPath: productionCompany.logo_path,
+                                        country: productionCompany.origin_country)
+            })
+            
+            return MovieDetailsEntity(id: dummyDetailsMovieInfo.id,
+                                      title: dummyDetailsMovieInfo.title,
+                                      posterPath: dummyDetailsMovieInfo.poster_path,
+                                      alternativeImage: dummyDetailsMovieInfo.backdrop_path,
+                                      tagline: dummyDetailsMovieInfo.tagline,
+                                      releaseDate: dummyDetailsMovieInfo.release_date,
+                                      voteAverage: dummyDetailsMovieInfo.vote_average,
+                                      originalLanguage: dummyDetailsMovieInfo.original_language,
+                                      genreList: genreList,
+                                      runTime: dummyDetailsMovieInfo.runtime,
+                                      overview: dummyDetailsMovieInfo.overview,
+                                      similarMoviesList: similarMoviesArray,
+                                      productionCompanies: productionCompaniesArray)
         }
     }
 }
