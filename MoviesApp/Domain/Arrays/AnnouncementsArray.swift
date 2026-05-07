a441aa8a9a64287e7d598bbd9cfef282a4c46787
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-05-06 23:34:10 -0600

    Fix: Removed URL with token and added a new one.

--- MoviesApp/Domain/Arrays/AnnouncementsArray.swift
+++ MoviesApp/Domain/Arrays/AnnouncementsArray.swift
@@ -8,5 +8,5 @@
 
 let announcementsArray: [String] = ["https://m.media-amazon.com/images/I/615UsSWBHxL._AC_UF894,1000_QL80_.jpg",
                                    "https://www.stophateuk.org/wp-content/uploads/2022/10/Stop-Hate-Racism_skin-colour-isnt-a-crime-1x1-1.png",
-                                   "https://etuceprodstorage.blob.core.windows.net/assets/image/1624/2605/lead.jpg?se=3026-02-01T23:17:58Z&sv=2024-08-04&sr=b&sig=GZcm05tXxupE57zY6mQZEDJdviVZRmWOXqIYnr8%2FtIQ%3D&sp=r",
+                                   "https://www.undp.org/sites/g/files/zskgke326/files/2024-11/orange_the_world_20212_0.jpg",
                                     "https://muselot.in/cdn/shop/products/Love-is-love-posters-design_2048x.jpg?v=1657309680"]
