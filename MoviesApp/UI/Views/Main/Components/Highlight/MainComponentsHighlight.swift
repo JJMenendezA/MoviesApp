0052f15fe31c957e671168a6f1b84f1b80a7a887
Juan José Menéndez Alarcón <juanjomea98@gmail.com>
2026-05-06 22:35:56 -0600

    Feat: Made structural changes to the MainComponentsHighlight.

--- MoviesApp/UI/Views/Main/Components/Highlight/MainComponentsHighlight.swift
+++ MoviesApp/UI/Views/Main/Components/Highlight/MainComponentsHighlight.swift
@@ -12,40 +12,39 @@ struct MainComponentsHighlight: View {
     @EnvironmentObject var router: Router
     var movie: Movie
     var body: some View {
-        ZStack {
-            if let moviePosterPath = movie.posterPath,
-               let movieImageURL = URL(string: "https://image.tmdb.org/t/p/w500") {
-                KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
-                    .resizable()
-                    .frame(height: 700)
-                    .overlay {
-                        Color.black.opacity(0.8)
-                    }
-                
-                VStack {
-                    Button(action: { router.navigateTo(.details(id: movie.id)) },
-                           label: {
-                        KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
-                            .resizable()
-                            .frame(width: 250, height: 350)
-                            .padding(.horizontal, 35)
-                            .padding(.top, 45)
-                    })
+        if let moviePosterPath = movie.posterPath,
+           let movieImageURL = URL(string: "https://image.tmdb.org/t/p/w500") {
+            KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
+                .resizable()
+                .frame(height: 700)
+                .overlay {
+                    Color.black.opacity(0.8)
+                }
+                .overlay {
                     VStack {
-                        Text("Random pick of the day 👍")
-                            .foregroundStyle(.white.opacity(0.8))
-                            .font(.caption)
-                        
-                        Text(movie.title)
-                            .multilineTextAlignment(.center)
-                            .frame(alignment: .center)
-                            .foregroundStyle(.white)
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
+                        Button(action: { router.navigateTo(.details(id: movie.id)) },
+                               label: {
+                            KFImage(movieImageURL.appendingPathComponent(moviePosterPath))
+                                .resizable()
+                                .frame(width: 250, height: 350)
+                                .padding(.horizontal, 35)
+                                .padding(.top, 45)
+                        })
+                        VStack {
+                            Text("Random pick of the day 👍")
+                                .foregroundStyle(.white.opacity(0.8))
+                                .font(.caption)
+                            
+                            Text(movie.title)
+                                .multilineTextAlignment(.center)
+                                .frame(alignment: .center)
+                                .foregroundStyle(.white)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                        } // :VStack
+                        .padding()
                     } // :VStack
-                    .padding()
-                } // :VStack
-            } // :ZStack
+                }
         }
     }
 }
